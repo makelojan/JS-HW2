54408cd7262da422b11e19957a0b23d4e52d94ea
makelojan <makelojann@gmail.com>
2021-08-17 06:15:02 +0300

    Add local-storage and toast features

--- script.js
+++ script.js
@@ -2,35 +2,36 @@ const UL = document.querySelector("ul");
 const INPUT = document.querySelector("input");
 const ADD_BUTTON = document.querySelector("button.add");
 
-if (localStorage.hasOwnProperty("tasks")) {
-
-} else {
-    localStorage.setItem("tasks", "");
-}
-
 function Check_Remove(e) {
     const LI = e.path[1];
     if (e.target.tagName == "BUTTON") {
+        tasks.splice(tasks.findIndex(task=>task.text==LI.children[1].innerHTML), 1);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
         LI.remove();
+        $(".bg-danger").toast("show");
     } else {
         if (LI.classList.contains("task")) {
             LI.classList.remove("task");
             LI.classList.add("checked");
-        } else {
+            tasks.find(task=>task.text == LI.children[1].innerHTML).checked = true;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        } else if (LI.classList.contains("checked")){
             LI.classList.remove("checked");
             LI.classList.add("task");
+            tasks.find(task=>task.text == LI.children[1].innerHTML).checked = false;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
         }
     }
 }
-function Add() {
+function Add(text, checked) {
     const LI = document.createElement("li");
-    LI.className = "task";
+    LI.className = checked ? "checked" : "task";
     const CHECK = document.createElement("span");
     CHECK.innerHTML = "&#10003";
     CHECK.className = "check";
     LI.appendChild(CHECK);
     const TEXT = document.createElement("span");
-    TEXT.innerHTML = INPUT.value;
+    TEXT.innerHTML = text;
     TEXT.className = "text";
     LI.appendChild(TEXT);
     const BUTTON = document.createElement("button");
@@ -40,5 +41,23 @@ function Add() {
     UL.appendChild(LI);
 }
 
+let tasks = [];
+if (localStorage.hasOwnProperty("tasks")) {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+} else {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+tasks.map(task=>Add(task.text, task.checked));
+
 UL.addEventListener("click", Check_Remove);
-ADD_BUTTON.addEventListener("click", Add);
+ADD_BUTTON.addEventListener("click", ()=>{
+    if (INPUT.value) {
+        Add(INPUT.value, false);
+        tasks.push({"text": INPUT.value, "checked": false});
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        INPUT.value = "";
+        $(".bg-success").toast("show");
+    } else {
+        $(".bg-warning").toast("show");
+    }
+});
